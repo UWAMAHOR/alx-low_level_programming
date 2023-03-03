cb16301d25562440618cd224b28cbad5e1ff97d5
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-03-03 13:31:52 -0800

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @str:string
+ * Return:capital strings
+ */
+char *cap_string(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a])
+	{
+		while (!(str[a] >= 'a' && str[a] <= 'z'))
+			a++;
+		if (str[a - 1] == ' ' ||
+				str[a - 1] == '\t' ||
+				str[a - 1] == '\n' ||
+				str[a - 1] == ',' ||
+				str[a - 1] == ';' ||
+				str[a - 1] == '.' ||
+				str[a - 1] == '!' ||
+				str[a - 1] == '?' ||
+				str[a - 1] == '"' ||
+				str[a - 1] == '(' ||
+				str[a - 1] == ')' ||
+				str[a - 1] == '{' ||
+				str[a - 1] == '}' ||
+				a == 0)
+			str[a] -= 32;
+		a++;
+	}
+	return (str);
+}
+
