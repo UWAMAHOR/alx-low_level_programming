ed13acc6ba1c21077c076ad59d2d687107527a0f
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-03-05 05:56:24 -0800

    functions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - list all multiples of 3 and 5 under 1024
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: character
+ * Return: 1 if n greater than zer or 0 if n is zero or -1 if n is less zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+
