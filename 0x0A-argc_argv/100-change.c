1f860d3bf055c6653d0eec553963cb509e22a05e
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-03-11 09:36:32 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,66 +1,40 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _atoi - converts string to int
- * @s:string
- * Return:converted int
+ * main - prints the minimum number of coins to make change for an amount.
+ * of money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int _atoi(char *s)
+
+int main(int argc, char *argv[])
 {
-	int i = 0;
-	int sign = 1;
-	int n = 0;
+	int cents, ncoins = 0;
 
-	if (s[0] == '-')
+	if (argc == 1 || argc > 2)
 	{
-		sign = -1;
-		i++;
-	}
-	for (; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		n = n * 10 + (s[i] - '0');
-		else
-			break;
-	}
-	return (sign * n);
-}
-
-/**
- * main - prints minimum number of coins
- * @argc:number of arguments
- * @argv:array of arguments
- * Return:always 0
- */
-int main(int argc, char *argv[]__attribute__((unused)))
-{
-	int cents = 0;
-	int coins = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
+	printf("Error\n");
+	return (1);
 	}
 
-	if (cents < 0)
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
 	{
-		printf("0\n");
-		return (0);
+	if (cents >= 25)
+		cents -= 25;
+	else if (cents >= 10)
+		cents -= 10;
+	else if (cents >= 5)
+		cents -= 5;
+	else if (cents >= 2)
+		cents -= 2;
+	else if (cents >= 1)
+		cents -= 1;
+	ncoins += 1;
 	}
-
-	coins += cents / 25;
-	cents %= 25;
-	coins += cents / 10;
-	cents %= 10;
-	coins += cents / 5;
-	cents %= 5;
-	coins += cents / 2;
-	cents %= 2;
-	coins += cents;
-
-	printf("%d\n", coins);
-
+	printf("%d\n", ncoins);
 	return (0);
 }
