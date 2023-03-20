a4c3e5f95b897aaecd0cdbeba9fc0835332ecc2f
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-03-20 12:41:04 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,26 +1,82 @@
+#include <stdlib.h>
 #include "dog.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
+ * _copy - Make a copy of passed in argument
+ * @src: Data to make copy of
+ * Return: Pointer
+ */
+
+char *_copy(char *src)
+{
+	char *ptr;
+
+	int i, len;
+
+	if (src == NULL)
+	{
+
+		return (NULL);
+
+	}
+
+	for (len = 0; src[len] != '\0'; len++)
+
+		;
+
+
+	ptr = malloc(sizeof(char) * (len + 1));
+
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		ptr[i] = src[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * new_dog - Create a new dog variable
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Pointer to new dog variable
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = (dog_t*) malloc(sizeof(dog_t));
+	dog_t *logan;
+	char *new_name, *new_owner;
 
-	if (new_dog == NULL)
+	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = (char*) malloc(strlen(name) + 1);
-	if (new_dog->name == NULL)
+	logan = malloc(sizeof(dog_t));
+	if (logan == NULL)
 	{
-		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->name, name);
-	
-	new_dog->owner = (char*) malloc(strlen(owner) + 1);
-	if (new_dog->owner == NULL)
+	new_name = _copy(name);
+	if (new_name == NULL)
 	{
+		free(logan);
+		return (NULL);
+	}
+	(*logan).name = new_name;
+	(*logan).age = age;
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*logan).name);
+		free(logan);
+		return (NULL);
+	}
+	(*logan).owner = new_owner;
+	return (logan);
+}
 
