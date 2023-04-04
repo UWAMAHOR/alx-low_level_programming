66d8d5211c998275dc0a4b04d5e20a6eb48382f0
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-04-04 01:57:46 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,22 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in
- * a linked list.
- * @h: head of a list.
+ * listint_len - returns the number of elements
+ * @h: head of node
  *
- *
- * Return: numbers of nodes.
+ * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nnodes++;
+		node_count++;
 	}
-	return (nnodes);
+	return (node_count);
 }
-
