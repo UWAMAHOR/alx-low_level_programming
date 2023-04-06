65ef891eb47026865c87a7a47d7687e20eb8e71d
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-04-06 04:27:03 -0700

    prints all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		node_count++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		n++;
 	}
-
-	return (node_count);
+	return (n);
 }
