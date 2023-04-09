c0f68b900e80d6ee472731d7e43b59966ee36a44
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-04-09 10:33:06 -0700

    reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and print to STDOUT
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t t, w;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	close(fd);
+
+	free(buf);
+
+	return (w);
+}
+
+
