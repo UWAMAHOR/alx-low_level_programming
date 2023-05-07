65141023bdcf5477de329859525a94957f28459d
UWAMAHOR <uwamahoropascaline11@gmail.com>
2023-05-07 08:41:51 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,22 +10,6 @@ void print_binary(unsigned long int n)
 	{
 		print_binary(n >> 1);
 	}
-	_putchar('0' + (n & 1));
+	_putchar((n & 1) + '0');
 }
 
-6.100-get_endianness.c
-
-#include "main.h"
-
-/**
- *  get_endianness - checks the endianness.
- *  Return: 0 if big endian, 1 if little endian
- */
-
-int get_endianness(void)
-{
-	unsigned int i = 1;
-	char *c = (char *) &i;
-
-	return (*c);
-}
